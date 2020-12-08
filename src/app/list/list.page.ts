3eb263f2d1c418c8fd59e71d62aa8755775ff147
Supporterino <lars@roth-kl.de>
2020-12-08 18:30:09 +0000

    Added searchbar

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -9,6 +9,8 @@ import { SearchService } from '../services/search.service';
 })
 export class ListPage implements OnInit {
   diseases: Disease[] = [];
+  diseasesStore: Disease[] = [];
+  searchTerm = '';
 
   constructor(private searchService: SearchService) { }
 
@@ -17,8 +19,16 @@ export class ListPage implements OnInit {
 
   ionViewWillEnter() {
     this.searchService.getAllDiseases().subscribe(data => {
-      this.diseases = data;
+      this.diseasesStore = data.sort((a,b ) => {
+        return (a.name.toLowerCase() < b.name.toLowerCase()) ? -1 : (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : 0;
+      });
+      this.searching();
     })
   }
 
+  searching() {
+    this.diseases = this.diseasesStore.filter(item => {
+      return item.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+    })
+  }
 }
