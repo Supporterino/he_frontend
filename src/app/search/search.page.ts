b0e463ceba20431700c0f0f4717428be7260b600
Supporterino <lars@roth-kl.de>
2020-12-08 20:25:15 +0000

    ?

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -13,6 +13,8 @@ export class SearchPage implements OnInit {
   myForm: FormGroup;
   selectedSigns: Set<string> = new Set<string>();
   diseases: Disease[] = [];
+  rawSigns: string[] = [];
+  signs: string[] = [];
 
   constructor(public signsService: SignsService, private searchService: SearchService) {
   }
@@ -27,7 +29,28 @@ export class SearchPage implements OnInit {
 
   ionViewWillEnter() {
     this.selectedSigns = new Set<string>();
-    this.diseases = []
+    this.diseases = [];
+    this.signsService.getAllSigns().subscribe(data => this.rawSigns = data);
+  }
+
+  initSigns() {
+    this.signs = this.rawSigns;
+  }
+
+  searchList(event) {
+    this.initSigns();
+    const val = event.target.value;
+    if (val && val.trim != '') {
+      this.signs = this.signs.filter(e => {
+        return (e.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    } else {
+      this.signs = [];
+    }
+  }
+
+  select(val) {
+    this.selectedSigns.add(val);
   }
 
   submit() {
